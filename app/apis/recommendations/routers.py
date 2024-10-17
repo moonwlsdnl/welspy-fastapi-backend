91acf86bfd692eb51399363298847aec792ed9f8
moonwlsdnl <moonwlsdnl@23s068.local>
2024-10-17 17:16:27 +0900

    Fix: fix cloudbuild.yaml

--- app/apis/recommendations/routers.py
+++ app/apis/recommendations/routers.py
@@ -1,17 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from app.dependencies import get_db, get_redis
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.apis.recommendations.schemas import RequestDto, ResponseDto
+from app.apis.recommendations.schemas import ResponseDto
 from app.apis.recommendations.service import RecommendationsService
 
 router = APIRouter()
 
-@router.post("/recommendations", response_model=ResponseDto)
-async def recommendations(request: RequestDto, db: AsyncSession = Depends(get_db), redis_client = Depends(get_redis)):
-    user_email = request.user_email
-    page = request.page
-    size = request.size
-
+@router.get("/recommendations", response_model=ResponseDto)
+async def recommendations(
+    user_email: str = Query(..., description="User email"),
+    page: int = Query(1, description="Page number"),
+    size: int = Query(10, description="Page size"),
+    db: AsyncSession = Depends(get_db),
+    redis_client = Depends(get_redis)
+):
     service = RecommendationsService(db, redis_client)
 
     # Redis에서 캐시된 데이터 확인
@@ -21,7 +23,7 @@ async def recommendations(request: RequestDto, db: AsyncSession = Depends(get_db
     if cached_data:
         paginated_challenges = await service.paginate_challenges(cached_data, page=page, size=size)
         return {"roomIds": paginated_challenges}
-    
+
     # 유사한 사용자
     similar_users = await service.get_similar_users(user_email)
     print(similar_users)
@@ -32,7 +34,7 @@ async def recommendations(request: RequestDto, db: AsyncSession = Depends(get_db
 
     # 모든 챌린지를 count하고 정렬
     sorted_challenges = await service.count_and_sort_challenges(challenges)
-    
+
     # 전체 챌린지를 Redis에 저장 (TTL 6시간)
     await service.cache_challenges(user_email, sorted_challenges)
 
--- app/apis/recommendations/schemas.py
+++ app/apis/recommendations/schemas.py
@@ -1,9 +1,4 @@
 from pydantic import BaseModel
 
-class RequestDto(BaseModel):
-    page: int
-    size: int
-    user_email: str
-
 class ResponseDto(BaseModel):
     roomIds: list[int]
